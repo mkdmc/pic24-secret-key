012164b35e8d9045e66c8ef5c072e4be6a1ae27d
Mehrshad Kavousi <mehrshad.kavoosi@hotmail.com>
2025-12-06 16:57:37 +0100

    Initial program - Draw 5 circles
    Draws 5 circles on the screen, representing the capacitative touch buttons.

---
+++ main.c
@@ -0,0 +1,118 @@
+#include "PIC24FStarter.h"
+
+// Delay function using Timer1
+void delay(unsigned int milliseconds) {
+    T1CONbits.TCKPS = 0b11; // Prescale 1:256
+    PR1 = 47; TMR1 = 0;     // Reset timer counter
+    T1CONbits.TON = 1;      // Turn on Timer1
+    unsigned int count = 0;
+    while (count < milliseconds) {
+        while (!IFS0bits.T1IF); // Wait for Timer1 interrupt flag
+        IFS0bits.T1IF = 0;      // Clear Timer1 interrupt flag
+        count++;
+    }
+    T1CONbits.TON = 0;      // Turn off Timer1
+}
+
+// Helper function to plot 8 symmetric points of a circle
+void plotPoints(uint8_t xctr, uint8_t yctr, uint8_t x, uint8_t y) {
+    PutPixel(xctr + x, yctr + y); PutPixel(xctr - x, yctr + y);
+    PutPixel(xctr + x, yctr - y); PutPixel(xctr - x, yctr - y);
+    PutPixel(xctr + y, yctr + x); PutPixel(xctr - y, yctr + x);
+    PutPixel(xctr + y, yctr - x); PutPixel(xctr - y, yctr - x);
+}
+
+// Draw a hollow circle at (x,y) with radius r
+void drawCircle(uint8_t x1, uint8_t y1, uint8_t r) {
+    uint8_t x = 0, y = r;
+    uint8_t p = 1 - r; 
+    plotPoints(x1, y1, x, y);
+    while (x < y) {
+        x++;
+        if (p < 0) p += 2 * x + 1;
+        else p += 2 * (x - --y) + 1;
+        plotPoints(x1, y1, x, y);
+    }
+}
+
+// Helper function to draw a filled circle (for pressed state)
+void drawFilledCircle(uint8_t x1, uint8_t y1, uint8_t r) {
+    uint8_t i;
+    for(i = 0; i <= r; i++) {
+        drawCircle(x1, y1, i);
+    }
+}
+
+// Screen Resolution is 128x64. Center is (64, 32).
+// Button Mapping:
+// Index 0: Up
+// Index 1: Right
+// Index 2: Down
+// Index 3: Left
+// Index 4: Center (Pin AN12/RB12)
+
+const uint8_t btnX[5] = {64, 104, 64, 24, 64}; // X positions
+const uint8_t btnY[5] = {12, 32, 52, 32, 32};  // Y positions
+const uint8_t RADIUS = 6;                      // Circle Radius
+
+int main(void) {
+    // Hardware Initialization
+    INIT_CLOCK(); 
+    CTMUInit(); 
+    RGBMapColorPins();
+    
+    RGBTurnOnLED();
+    ResetDevice();
+    ClearDevice();
+    
+    // Array to track the previous state of buttons to optimize drawing
+    // Initialize to 0xFF to force an initial draw
+    uint8_t prev_buttons[5] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    
+    while(1) { 
+        // Read touch sensors; updates the global 'buttons' array
+        ReadCTMU(); 
+        
+        // Loop through all 5 buttons
+        for (uint8_t i = 0; i < 5; i++) {
+            
+            // Only redraw if the state of the button has changed
+            if (buttons[i] != prev_buttons[i]) {
+                
+                // 1. Erase the old shape
+                // We draw a black filled circle to clear the area completely
+                SetColor(BLACK);
+                drawFilledCircle(btnX[i], btnY[i], RADIUS);
+                
+                // 2. Draw the new shape
+                SetColor(WHITE);
+                
+                if (buttons[i]) {
+                    // STATE: PRESSED
+                    // Draw a solid white circle (Active node)
+                    drawFilledCircle(btnX[i], btnY[i], RADIUS);
+                } else {
+                    // STATE: RELEASED
+                    // Draw a hollow circle with a center dot (Inactive node)
+                    drawCircle(btnX[i], btnY[i], RADIUS);
+                    PutPixel(btnX[i], btnY[i]); 
+                }
+                
+                // Update state tracker
+                prev_buttons[i] = buttons[i];
+            }
+        }
+        
+        // Light up RGB LED Green if any button is pressed
+        uint8_t anyPressed = 0;
+        for(uint8_t j=0; j<5; j++) { if(buttons[j]) anyPressed = 1; }
+        
+        if(anyPressed) SetRGBs(0, 255, 0); // Green
+        else SetRGBs(0, 0, 0);             // Off
+
+        delay(10); // Short delay to debounce and limit refresh rate
+    }
+    
+    RGBTurnOffLED();
+    return 0;
+}
---
+++ nbproject/Makefile-default.mk
@@ -0,0 +1,189 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
+include Makefile
+# Include makefile containing local settings
+ifeq "$(wildcard nbproject/Makefile-local-default.mk)" "nbproject/Makefile-local-default.mk"
+include nbproject/Makefile-local-default.mk
+endif
+endif
+
+# Environment
+MKDIR=mkdir -p
+RM=rm -f 
+MV=mv 
+CP=cp 
+
+# Macros
+CND_CONF=default
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+OUTPUT_SUFFIX=elf
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/pic24fStarter.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+else
+IMAGE_TYPE=production
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/pic24fStarter.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+endif
+
+ifeq ($(COMPARE_BUILD), true)
+COMPARISON_BUILD=-mafrlcsj
+else
+COMPARISON_BUILD=
+endif
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Source Files Quoted if spaced
+SOURCEFILES_QUOTED_IF_SPACED=SH1101A.c main.c TouchSense.c RGBLeds.c
+
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/SH1101A.o ${OBJECTDIR}/main.o ${OBJECTDIR}/TouchSense.o ${OBJECTDIR}/RGBLeds.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/SH1101A.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/TouchSense.o.d ${OBJECTDIR}/RGBLeds.o.d
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/SH1101A.o ${OBJECTDIR}/main.o ${OBJECTDIR}/TouchSense.o ${OBJECTDIR}/RGBLeds.o
+
+# Source Files
+SOURCEFILES=SH1101A.c main.c TouchSense.c RGBLeds.c
+
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+
+.build-conf:  ${BUILD_SUBPROJECTS}
+ifneq ($(INFORMATION_MESSAGE), )
+	@echo $(INFORMATION_MESSAGE)
+endif
+	${MAKE}  -f nbproject/Makefile-default.mk ${DISTDIR}/pic24fStarter.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+
+MP_PROCESSOR_OPTION=24FJ256GB106
+MP_LINKER_FILE_OPTION=,--script=p24FJ256GB106.gld
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/SH1101A.o: SH1101A.c  .generated_files/flags/default/9504c0860e346b749f8a0d13ec26039f65b13e9e .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/SH1101A.o.d 
+	@${RM} ${OBJECTDIR}/SH1101A.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  SH1101A.c  -o ${OBJECTDIR}/SH1101A.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/SH1101A.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/9a06161e05ab09841b465aa2be441f827fdc735b .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/main.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/TouchSense.o: TouchSense.c  .generated_files/flags/default/9ec3bea5eb2f3e3f321e10ea6504d7e6b780e21d .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/TouchSense.o.d 
+	@${RM} ${OBJECTDIR}/TouchSense.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  TouchSense.c  -o ${OBJECTDIR}/TouchSense.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/TouchSense.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/RGBLeds.o: RGBLeds.c  .generated_files/flags/default/8485f27bd1c0a6c403e176020077c365d74a9ac6 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/RGBLeds.o.d 
+	@${RM} ${OBJECTDIR}/RGBLeds.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  RGBLeds.c  -o ${OBJECTDIR}/RGBLeds.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/RGBLeds.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+else
+${OBJECTDIR}/SH1101A.o: SH1101A.c  .generated_files/flags/default/f0ae9f58e18b12dfa49324dc7db98c4d182b7dcf .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/SH1101A.o.d 
+	@${RM} ${OBJECTDIR}/SH1101A.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  SH1101A.c  -o ${OBJECTDIR}/SH1101A.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/SH1101A.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/76c79c622f3f7701c55f6e52c5f1a792fde0b169 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/main.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/TouchSense.o: TouchSense.c  .generated_files/flags/default/59ea864538e43cdea31127cf44e6de6ab44c48c7 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/TouchSense.o.d 
+	@${RM} ${OBJECTDIR}/TouchSense.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  TouchSense.c  -o ${OBJECTDIR}/TouchSense.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/TouchSense.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/RGBLeds.o: RGBLeds.c  .generated_files/flags/default/da61b59ecd3430607e0ea02250d7006f6b4391bb .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/RGBLeds.o.d 
+	@${RM} ${OBJECTDIR}/RGBLeds.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  RGBLeds.c  -o ${OBJECTDIR}/RGBLeds.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/RGBLeds.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemblePreproc
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${DISTDIR}/pic24fStarter.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o ${DISTDIR}/pic24fStarter.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -D__DEBUG=__DEBUG   -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)   -mreserve=data@0x800:0x84F   -Wl,,,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D__DEBUG=__DEBUG,,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,${DISTDIR}/memoryfile.xml$(MP_EXTRA_LD_POST)  -mdfp="${DFP_DIR}/xc16" 
+	
+else
+${DISTDIR}/pic24fStarter.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o ${DISTDIR}/pic24fStarter.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -Wl,,,--defsym=__MPLAB_BUILD=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,${DISTDIR}/memoryfile.xml$(MP_EXTRA_LD_POST)  -mdfp="${DFP_DIR}/xc16" 
+	${MP_CC_DIR}/xc16-bin2hex ${DISTDIR}/pic24fStarter.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} -a  -omf=elf   -mdfp="${DFP_DIR}/xc16" 
+	
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+
+# Subprojects
+.clean-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${OBJECTDIR}
+	${RM} -r ${DISTDIR}
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(wildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
